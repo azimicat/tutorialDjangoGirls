9266334b92360a7011bee168b012378bcdd9dddb
azimicat <asahi-nakamura@azimicat.dev>
2022-04-18 18:45:33 +0900

    feat: サーバーが起動するまで

--- tutorialDjangoGirls/settings.py
+++ tutorialDjangoGirls/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vj7gjxx_-_iba^5uteipwvg&niq+rdt7+6jx#f$67angp*&gz*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [['0.0.0.0', '.pythonanywhere.com']]
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
