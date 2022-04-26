60b1564a9fd14ca38eda68e3a19fee3bec5638b0
azimicat <asahi-nakamura@azimicat.dev>
2022-04-26 17:52:13 +0900

    [feat] update

--- tutorialDjangoGirls/settings.py
+++ tutorialDjangoGirls/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vj7gjxx_-_iba^5uteipwvg&niq+rdt7+6jx#f$67angp*&gz*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'azimicat.pythonanywhere.com']
 
 
 # Application definition
